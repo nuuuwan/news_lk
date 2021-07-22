30a004432fdd53c31e5f632eb28cd8c8b3ad4b83
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-07-22 14:26:18 +0530

    Added PyPI files

---
+++ src/news_lk/_constants.py
@@ -0,0 +1,4 @@
+"""Constants."""
+
+CACHE_NAME = 'news_lk'
+CACHE_TIMEOUT = 3600
---
+++ src/news_lk/run_news_lk.py
@@ -0,0 +1,17 @@
+"""Implements run_news_lk."""
+
+
+def run_news_lk():
+    """Implements run_news_lk."""
+    return True
+
+def _dump():
+    """Dump data."""
+    return True
+
+def _load():
+    """Load data."""
+    return True
+
+if __name__ == '__main__':
+    run_news_lk()
