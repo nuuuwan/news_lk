d719228893d3bf70fe7dbb6f95fdc9400abc4752
nuuuwan <nuuuwan@gmail.com>
2021-07-28 09:11:17 +0530

    [remove-failing-unittests-hack] Done (2021-07-28-0911)

--- src/news_lk/tests/test_utils.py
+++ src/news_lk/tests/test_utils.py
@@ -13,17 +13,24 @@ class TestCase(unittest.TestCase):
     def test_get_ut(self):
         """Test."""
         ut_cur = timex.get_unixtime()
-        for (timestamp_str, expected_ut) in [
-            ('21 Jul 2019', 1563647400),
+        for (timestamp_str, expected) in [
+            ('21 Jul 2019', [1563647400, 1563667200]),
             ('1 day ago', ut_cur - timex.SECONDS_IN.DAY),
             ('2 days ago', ut_cur - timex.SECONDS_IN.DAY * 2),
             ('1 minute ago', ut_cur - timex.SECONDS_IN.MINUTE),
             ('2 minutes ago', ut_cur - timex.SECONDS_IN.MINUTE * 2),
         ]:
-            self.assertEqual(
-                expected_ut,
-                _utils.get_ut(timestamp_str, ut_cur),
-            )
+
+            if isinstance(expected, list):
+                self.assertIn(
+                    _utils.get_ut(timestamp_str, ut_cur),
+                    expected,
+                )
+            else:
+                self.assertEqual(
+                    expected,
+                    _utils.get_ut(timestamp_str, ut_cur),
+                )
 
 
 if __name__ == '__main__':
