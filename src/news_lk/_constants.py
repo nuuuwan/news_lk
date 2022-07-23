cd08aff8c075cbfc4ee54fb05307448d97e49ab7
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-23 14:15:12 +0530

    Initial

--- src/news_lk/_constants.py
+++
@@ -1,4 +0,0 @@
-"""Constants."""
-
-CACHE_NAME = 'news_lk'
-CACHE_TIMEOUT = 3600
--- src/news_lk/upload_data.py
+++
@@ -1,6 +0,0 @@
-"""Uploaded data to nuuuwan/news_lk:data branch."""
-
-from news_lk import scrape
-
-if __name__ == '__main__':
-    scrape.scrape_and_dump()
